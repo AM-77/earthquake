cdd8522fead94f6630e4452213b0a7b6f162080d
AM-77 <amine.griche77@protonmail.ch>
2020-08-25 04:16:11 -0500

    update the log message

--- center-service/index.js
+++ center-service/index.js
@@ -9,4 +9,4 @@ app.use(express.json())
 app.use(require("./app/routes"))
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => console.log(`The app is 🏃🏃🏃 on 🚪:${PORT}`))
+app.listen(PORT, () => console.log(`The center service is 🏃🏃🏃 on 🚪:${PORT}`))
