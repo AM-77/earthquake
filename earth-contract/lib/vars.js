d3d0d009e3b5fe9998ef41f81b7c5490bff9dd03
AM-77 <amine.griche77@protonmail.ch>
2020-08-25 23:51:01 -0500

    updated addresses

--- earth-contract/lib/vars.js
+++ earth-contract/lib/vars.js
@@ -1,7 +1,7 @@
 /* -!- DONT UPDATE THIS FILE MANUALY NO MATTER WAHT -!- */
-const smartContractAddress = "0x76c1f95F4170270afd1f039E8ec9A52AcaA92e97"
-const centerAddress = "0xE82E936b8A405d32410A6F603433724d644CD070"
-const mediaAddress = "0x8438ff95C9223844467DB8CE326D263466107761"
-const lawAddress = "0x40548C5Eb17e67505482D11964cB5BebdFA22F8A"
-const civilAddress = "0x6c84345E838Fbc509f94399d8FB2C6895584C1a0"
+const smartContractAddress = "0x241cc5905155e2dA774Cb994ACc5dfA7064b347F"
+const centerAddress = "0xc7B409D3432e55ede4DA7Fa4edEBB12dd56891D5"
+const mediaAddress = "0xabf67f17778b100243Ec918998670B12D0000b03"
+const lawAddress = "0xdDf9518F491cB8B05d28bBC8a5a8c14F92f88649"
+const civilAddress = "0x0e9729754D9dd4150Aef7d9A41f48e534076f6cA"
 module.exports = { smartContractAddress, centerAddress, mediaAddress, lawAddress, civilAddress }
